d8bad6b003a6d30e3fa2cab7722ab5bb21eb1127
frficc <info@frfi.io>
2022-12-13 18:17:05 +0300

    create the build-script

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -1,9 +1,59 @@
 // example from: https://github.com/decentraland/marketplace/tree/master/indexer
+const addressesJson = require('../addresses.json');
+const fs = require('node:fs/promises');
 
 const autofarmApiUrl = 'https://backend-api-prod.frfi.io';
 
+const Networks = {
+  56: 'bsc',
+};
+
 const getContracts = async () => {
-  const response = await fetch(`${baseUrl}/autofarm?$limit=200&${params}`);
+  const response = await fetch(`${autofarmApiUrl}/autofarm?$limit=200`);
   if (!response.ok) throw new Error('no response from vault config service');
   return response.json().then((data) => data?.data);
 };
+
+const main = async () => {
+  const yamlTemplate = await fs.readFile('./subgraph.template.yaml', { encoding: 'utf-8' });
+  const contracts = await getContracts();
+  console.log('contracts fetched');
+  const dataSources = [];
+  contracts.forEach((contract) => {
+    const { vaultAddress, chainId } = contract;
+    const network = Networks[chainId];
+    const addressConfig = addressesJson[chainId][vaultAddress];
+    if (!network) {
+      console.warn('no network of', chainId);
+      return;
+    }
+    if (!addressConfig) {
+      console.warn('no config of', vaultAddress);
+      return;
+    }
+    if (!addressConfig.startBlock) {
+      console.warn('no start block of', vaultAddress);
+      return;
+    }
+    const template = yamlTemplate
+      .replace('{{name}}', addressConfig.name)
+      .replace('{{network}}', network)
+      .replace('{{address}}', vaultAddress)
+      .replace('{{startBlock}}', addressConfig.startBlock);
+    dataSources.push(template);
+  });
+  await fs.writeFile(
+    './subgraph.yaml',
+    `
+specVersion: 0.0.5
+schema:
+  file: ./schema.graphql
+dataSources:
+${dataSources.join('\n')}
+`,
+    { encoding: 'utf-8' },
+  );
+  console.log('done');
+};
+
+main();
